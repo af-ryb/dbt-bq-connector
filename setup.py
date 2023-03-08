a632c5a4cc9309097621cc8edb773f38a91bf4bd
anatoliy.rybakov <anatoliy.rybakov@hitapps.com>
2023-03-08 09:06:48 +0100

    set version for db core as 1.5.0b2

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ def _dbt_core_version(plugin_version: str) -> str:
 
 package_name = "dbt-bigquery"
 package_version = "1.5.0b1"
-dbt_core_version = _dbt_core_version(_dbt_bigquery_version())
+dbt_core_version = "1.5.0b2"
 description = """The BigQuery adapter plugin for dbt"""
 
 setup(
@@ -74,7 +74,7 @@ setup(
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     include_package_data=True,
     install_requires=[
-        f"dbt-core~={_dbt_core_version(_dbt_bigquery_version())}",
+        f"dbt-core==1.5.0b2",
         "google-cloud-bigquery~=3.0",
         "google-cloud-storage~=2.4",
         "google-cloud-dataproc~=5.0",
